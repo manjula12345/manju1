412ea24ad0657e37506612d9cb1aebacc2acd337
manjula12345 <30342659+manjula12345@users.noreply.github.com>
2017-08-04 16:55:32 +0530

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int c;
+printf("enter the number");
+scanf("%d",&c);
+if(c/2==0)
+{
+printf("the number is even");
+}
+else
+{
+printf("the number is odd");
+}
+return 0;
+}
